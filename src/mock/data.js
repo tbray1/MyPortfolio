645f63de67c392867508b816781288d9934b9f54
tbray <tariusbray06@gmail.com>
2021-04-19 15:51:42 -0400

    update

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,47 +19,57 @@ export const heroData = {
 export const aboutData = {
   img: 't.jpg',
   paragraphOne:
-    'Hi, My name is Tarius Bray and I recently found a passion for building web applications. Im comfortable working with front end technologies such as ReactJS, HTML, CSS, and JS. I have also been using java for 3+ years professionaly.',
+    'Hi, My name is Tarius Bray and I enjoy building high-quality web applications . Im comfortable working with a range of web technologies such as Liquid, JavaScript, Java, HTML, and CSS.',
   paragraphTwo:
-    'I have worked as a QA engineer for 4 years, mostly writing automation test suites using java.',
+    'Although Im new to freelancing I have a deep engineering background as ive been working professionaly as a QA engineer for 5+ years testing complex enterprise web applications. I also have a masters degree in Software Engineering from Kennesaw State University(2020)',
   paragraphThree:
     'Check out some of my latest work below. Please dont hesitate to reach out if my skills align with your business needs.',
   // if no resume, the button will not show up
 };
-
-// PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project1.png',
-    title: 'NBA cards Application ',
-    info:
-      'In my spare time I really enjoy watching NBA Basketball, So I decided to start working on this NBA Cards application to make it easier for me to keep up with my favorite teams. The application fetches NBA data from an external api and displays the data as cards.',
-    info2: 'Tech Stack- ReactJS(Hooks, functional components, axios), JSX, CSS, GIT',
-    url: 'https://nba-cards.netlify.app',
-    repo: 'https://github.com/tbray1/NBA_Cards',
+    img: 'sneaks.png',
+    title: 'Sneaks Shopify Development Store ',
+    info: 'password - 123',
+    url: 'https://4ep34ey1sjym7tf7-56178868394.shopifypreview.com/',
+  },
+  {
+    id: nanoid(),
+    img: 'winewithamber.png',
+    title: 'Website for Handcrafted Wine Maker ',
+    info: 'Tech Stack- HTML, JS and Tailwind CSS ',
+    url: 'https://winewithamber.com',
+    repo: 'https://github.com/tbray1/WineWithAmber', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project2.png',
     title: 'Wikipedia Search App ',
-    info:
-      'This application takes user input and fetches data from Wikipedia api based on the given input.',
-    info2: 'Tech Stack- ReactJS(Functional Components, Hooks, JSX), SASS, GIT',
+    info: 'Tech Stack- ReactJS, SASS, GIT',
     url: 'https://wikipedia-searchapp.netlify.app',
     repo: 'https://github.com/tbray1/WikipediaSearch', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project3.png',
-    title: 'Portfolio Project',
-    info:
-      'For this portfolio project i used a gatsby starter built using react, i tweaked the styling to suit my personal needs',
-    info2: 'gatsby, sass, git',
+    img: 'corona.png',
+    title: 'Covid Tracker Project',
+    info: 'Tech Stack- Angular, HTML, CSS',
+    info2: '',
     url: 'tariusbray.com',
     repo: 'https://github.com/tbray1/MyPortfolio', // if no repo, the button will not show up
   },
+  {
+    id: nanoid(),
+    img: 'project1.png',
+    title: 'NBA cards Application ',
+    info: '',
+    info2: 'Tech Stack- ReactJS, JSX, CSS, GIT',
+    url: 'https://nba-cards.netlify.app',
+    repo: 'https://github.com/tbray1/NBA_Cards',
+  },
 ];
+// PROJECTS DATA
 
 // CONTACT DATA
 export const contactData = {
